d2ff9fa37ef61c9478468243d99551d213dc9b8b
Originalchik15 <136095159+Originalchik15@users.noreply.github.com>
2024-11-22 13:24:45 +0400

    Update lab3.py

--- l3/lab3.py
+++ l3/lab3.py
@@ -15,46 +15,28 @@ def valid(word):
     if word[0] == '-':
         word = word[1:]  
     return len(word) > 0 and word[0] == '3' and all('0' <= ch <= '7' for ch in word) and int(word, 8) % 2 == 1
-
 def transform_number(num):
     dc = {}
     trans = []
-    ch = 0
     is_negative = num[0] == '-' 
     if is_negative:
         num = num[1:]  
-
     for d in num:
         if d in dc and d in inter:
             trans.append(inter[d])
-            ch += 1
         else:
             trans.append(d)
             dc[d] = 1
-
     result = ''.join(trans)
     if is_negative:
         result = '-' + result  
-
-    if ch > 0:
-        return result
-    else:
-        return ''
-
+    return result 
 def proc(path):
-    res = []
     with open(path, 'r', encoding='utf-8') as file:
         for line in file:
-            words = line.split() 
-            trans_line = [] 
-            for word in words:
-                if valid(word):
-                    trans_line.append(transform_number(word))
-                else:
-                    continue
-                    
-            res.append(' '.join(trans_line))
-    for r in res:
-        print(r)
+            words = line.split()  
+            result = ' '.join(transform_number(word) for word in words if valid(word))
+            if result:
+                print(result.strip()) 
 
 proc('inp.txt')
