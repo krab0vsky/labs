8320043ea0c63a9db785c5ceff7f9c8bdfb494d0
Originalchik15 <136095159+Originalchik15@users.noreply.github.com>
2024-12-03 11:20:38 +0400

    Add files via upload

---
+++ l6/lab6.1.py
@@ -0,0 +1,41 @@
+'''Задание состоит из двух частей. 
+1 часть – написать программу в соответствии со своим вариантом задания. Написать 2 варианта формирования (алгоритмический и с помощью функций Питона), сравнив по времени их выполнение.
+2 часть – усложнить написанную программу, введя по своему усмотрению в условие минимум одно ограничение на характеристики объектов (которое будет сокращать количество переборов) и целевую функцию для нахождения оптимального  решения.
+Вариант 19. Для участия в команде тренер отбирает 5 мальчиков из 10. Вывести все возможные варианты состава команды.'''
+from itertools import combinations
+import timeit
+boys = ["Иван", "Петр", "Сергей", "Алексей", "Дмитрий", "Максим", "Андрей", "Константин", "Виктор", "Никита"]
+def m1(boys):
+    teams = []
+    n = len(boys)
+    for i in range(n):
+        for j in range(i + 1, n):
+            for m in range(j + 1, n):
+                for p in range(m + 1, n):
+                    for q in range(p + 1, n):
+                        team = (boys[i], boys[j], boys[m], boys[p], boys[q])
+                        teams.append(team)
+    return teams
+
+def m2(boys):
+    teams = list(combinations(boys,5))
+    return teams
+ch = 0
+while True:
+    print("С чем работаем\n1-Заранее сгенерированный список\n2-Заполнить самому")
+    ch = int(input("Ваш выбор: "))
+    if ch == 1 or ch == 2:
+        break
+    else:
+        print("Неверный выбор")
+
+if ch == 2:
+    boys = []
+    for i in range(10):
+        boys.append(input("Введите имя: "))
+algorithmic_time = timeit.timeit('m1(boys)', globals=globals(), number=1000)
+itertools_time = timeit.timeit('m2(boys)', globals=globals(), number=1000)
+print(f"Алгоритмически\n{m1(boys)}")
+print(f"Время выполнения алгоритмического подхода: {algorithmic_time:.6f} секунд")
+print(f"Функциями питона\n{m2(boys)}")
+print(f"Время выполнения подхода с itertools: {itertools_time:.6f} секунд")
---
+++ l6/lab6.2.py
@@ -0,0 +1,60 @@
+'''Задание состоит из двух частей. 
+1 часть – написать программу в соответствии со своим вариантом задания. Написать 2 варианта формирования (алгоритмический и с помощью функций Питона), сравнив по времени их выполнение.
+2 часть – усложнить написанную программу, введя по своему усмотрению в условие минимум одно ограничение на характеристики объектов (которое будет сокращать количество переборов) и целевую функцию для нахождения оптимального  решения.
+Вариант 19. Для участия в команде тренер отбирает 5 мальчиков из 10. Вывести все возможные варианты состава команды.
+Условие ограничения: если name1 в команде, name2 не может быть
+'''
+
+
+from itertools import combinations
+import timeit
+
+boys = ["Иван", "Петр", "Сергей", "Алексей", "Дмитрий", "Максим", "Андрей", "Константин", "Виктор", "Никита"]
+name1 = "Сергей"
+name2 = "Андрей"
+def m1(boys):
+    teams = []
+    n = len(boys)
+    for i in range(n):
+        for j in range(i + 1, n):
+            for m in range(j + 1, n):
+                for p in range(m + 1, n):
+                    for q in range(p + 1, n):
+                        team = (boys[i], boys[j], boys[m], boys[p], boys[q])
+                        if name1 in team and name2 in team:
+                            continue
+                        teams.append(team)
+    return teams
+
+def m2(boys):
+    teams = []
+    for team in combinations(boys, 5):
+        if name1 in team and name2 in team:
+            continue
+        teams.append(team)
+    return teams
+
+ch = 0
+while True:
+    print("С чем работаем\n1-Заранее сгенерированный список\n2-Заполнить самому")
+    ch = int(input("Ваш выбор: "))
+    if ch == 1 or ch == 2:
+        break
+    else:
+        print("Неверный выбор")
+
+if ch == 2:
+    boys = []
+    for i in range(10):
+        boys.append(input("Введите имя: "))
+    print("Введите 2 имени которые не могут быть в одной команде")
+    name1 = input("Имя 1: ")
+    name2 = input("Имя 2: ")
+
+algorithmic_time = timeit.timeit('m1(boys)', globals=globals(), number=1000)
+itertools_time = timeit.timeit('m2(boys)', globals=globals(), number=1000)
+
+print(f"Алгоритмически\n{m1(boys)}")
+print(f"Время выполнения алгоритмического подхода: {algorithmic_time:.6f} секунд")
+print(f"Функциями питона\n{m2(boys)}")
+print(f"Время выполнения подхода с itertools: {itertools_time:.6f} секунд")
