a8987b2fb09abf60e118993b1aaf5311857fdab5
Originalchik15 <136095159+Originalchik15@users.noreply.github.com>
2024-12-17 11:35:35 +0400

    Add files via upload

--- l5/lab5.py
+++ l5/lab5.py
@@ -10,21 +10,18 @@
 import math
 import timeit
 
-def factorial(n, last_value=None):
+def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
-        current_value = n * factorial(n - 1, last_value)
-        last_value = current_value
-        return current_value
-    
-def recursion(n,sign=1,last_value=None):
+        return n * factorial(n - 1)
+
+def recursion(n):
     if n == 0 or n == 1:
         return 1
     else:
-        current_value = sign * (recursion(n-1,-sign, last_value) / (factorial(2*n) * recursion(n-2,-sign, last_value) + 2))
-        last_value = current_value
-        return current_value
+        min = 1 if n % 2 == 0 else -1
+        return min * (recursion(n - 1) / (factorial(2 * n) * recursion(n - 2) + 2))
 
 def iteration(n):
     if n == 0 or n == 1:
@@ -32,18 +29,23 @@ def iteration(n):
     else:
         prev1 = 1
         prev2 = 1  
-        last_value = 1  
-        sign = 1
+        last_value = 1
+        factorial = 2 
         for i in range(2, n + 1):
-            last_value = sign * (prev1 / (math.factorial(2 * i) * prev2 + 2))
+            f1 = 2*i-1
+            f2 = 2 * i
+            factorial *= f1 * f2
+            min = 1 if i % 2 == 0 else -1
+            last_value = min * (prev1 / (factorial * prev2 + 2))
             prev2 = prev1
             prev1 = last_value
-            sign *= -1
         return last_value
+
 n = int(input("Введите n: "))
 recursion_time = timeit.timeit(lambda: recursion(n), number=1000)
 iteration_time = timeit.timeit(lambda: iteration(n), number=1000)
+
 print(f"Результат рекурсии для n={n}: {recursion(n)}")
 print(f"Результат итерации для n={n}: {iteration(n)}")
 print(f"Время выполнения рекурсии: {recursion_time:.6f} секунд")
-print(f"Время выполнения итерации: {iteration_time:.6f} секунд")    
+print(f"Время выполнения итерации: {iteration_time:.6f} секунд") 
